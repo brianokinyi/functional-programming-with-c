6503a6deb33520b2cf779c87f7268632ae6785ca
brianokinyi <brianokinyi.bo@gmail.com>
2018-08-30 07:12:41 +0300

    Using realloc()

--- memoryAllocation.c
+++ memoryAllocation.c
@@ -4,7 +4,7 @@
 int main() {
     int num, i, *ptr, sum=0;
 
-    printf("Enter number of elements");
+    printf("Enter number of elements\n");
     scanf("%d", &num);
 
     ptr = (int*) malloc(num * sizeof(int));
@@ -12,13 +12,32 @@ int main() {
         printf("Error! Memory not allocated");
         exit(0);
     }
-    printf("Enter elements of array");
+    printf("Enter elements of array\n");
     for(i=0; i<num; i++){
         scanf("%d", ptr+i);
         sum+=*(ptr+i);
     }
 
-    printf("Sum = %d", sum);
+    printf("Sum = %d\n", sum);
+    // Reallocate using realloc(ptr, newSize)
+    sum=0;
+    printf("Enter new size of array\n");
+    scanf("%d", &num);
+
+    ptr = realloc(ptr, num);
+    if(ptr == NULL){
+        printf("Error! Memory not allocated\n");
+        exit(0);
+    }
+
+    printf("Enter elements of arr\n");
+    for(i=0; i<num; i++){
+        scanf("%d", ptr+i);
+        sum+= *(ptr+i);
+    }
+    printf("Sum = %d\n", sum);
+
     free(ptr);
+
     return 0;
 }
